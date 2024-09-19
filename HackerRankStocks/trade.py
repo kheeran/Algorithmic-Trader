64792a539ec4b2ae946f4e3182f39c5dedc344b2
Kheeran <kn16063@bristol.ac.uk>
2024-09-19 19:48:45 +0100

    continued filling in code

--- HackerRankStocks/trade.py
+++ HackerRankStocks/trade.py
@@ -20,7 +20,7 @@ class streamSim:
         pass
 
 
-# streaming algorithm
+# streaming algorithm for trading
 class myTrader:
     def __init__(self, mystocks : list, init_cash : int, max_hist_len : int, analysis = False):
 
@@ -30,56 +30,35 @@ class myTrader:
         self.max_hist_len = max_hist_len
         self.analysis = analysis
             
-        # key info store
-        self.mymodels = {} # current set of regression models (for each stock)
-        self.myprofit_total = {} # tracking the total profit of each stock so far
-        # 
-        self.cur_stocks_held = {} # each key is a stock and each value is a list of tuples (num, price). Can make it just a single tuple (instead of a list of tuples) 
-        # 
+        # key variables stored
         self.cur_day_count = 0
-        self.yesterday_prices = None
-        self.today_prices = None
-        self.today_prices_diff = None
-        self.tomorrow_preds = None
-        self.today_trades = None
+        self.yesterday_prices = {}
+        self.today_prices = {}
+        self.today_prices_diff = {}
+        self.tomorrow_preds = {}
+        self.today_sells = {}
+        self.today_buys = {}
+        self.mymodels = {} # current set of regression models (for each stock)
+        self.myprofit_total = { stock : 0 for stock in self.mystocks } # tracking the total profit of each stock so far
+        self.cur_stocks_held = { stock : 0 for stock in self.mystocks }  
 
-        # key data store
-        init_dict = {}
-        for stock in self.mystocks:
-            init_dict[stock] = [ None ]
-        self.stored_prices = copy.deepcopy(init_dict)
-        self.stored_prices_diff = copy.deepcopy(init_dict)
-        self.stored_prices_tomorrow = copy.deepcopy(init_dict)
+        # key arrays stored (over many days, inclulding initial day 0 of no activity/initialisation)
+        init_dict = { stock : [ None ] for stock in self.mystocks }
+        self.stored_prices = { stock : [ None ] for stock in self.mystocks }
+        self.stored_prices_diff = { stock : [ None ] for stock in self.mystocks }
+        self.stored_prices_tomorrow = { stock : [ None ] for stock in self.mystocks }
         if self.analysis:
             self.mydays_daily = [ self.cur_day_count ]
-            self.mysells_daily = copy.deepcopy(init_dict)
-            self.myprofits_daily = copy.deepcopy(init_dict)
-            self.mypredictions_daily = copy.deepcopy(init_dict)
-            self.mybuys_daily = copy.deepcopy(init_dict)
+            self.mysells_daily = { stock : [ None ] for stock in self.mystocks }
+            self.myprofits_daily = { stock : [ None ] for stock in self.mystocks }
+            self.mypredictions_daily = { stock : [ None ] for stock in self.mystocks }
+            self.mybuys_daily = { stock : [ None ] for stock in self.mystocks }
             self.mycash_daily = [ self.cur_cash ] # at end of day
 
-    # initialise day
-    def initDay(self, today_prices) -> None:
-        self.cur_day_count += 1
-        self.yesterday_prices = self.today_prices
-        self.today_prices = today_prices
-        self.today_prices_diff = {}
-        self.tomorrow_preds = None
-        self.today_trades = None
+    
 
-        # set type of prices, check validity, and compute price difference
-        for stock in self.mystocks:
-            try:
-                self.today_prices[stock] = float(self.today_prices[stock])
-            except:
-                raise TypeError("Number string contians invalid characters. Can not convert to float!")
 
-            self.today_prices_diff[stock] = self.today_prices[stock] - self.yesterday_prices[stock]
-
-        if self.analysis:
-            self.mydays_daily.append(self.cur_day_count)
-
-    # train pred models for each stock
+    # train prediction models for each stock
     def trainModels(self) -> None:
 
         # create dataframe
@@ -117,15 +96,8 @@ class myTrader:
             self.mymodels[stock] = model
 
         # NOTE: Could possibly make a continuous update version with own implemention of decision trees. E.g., after every 50 days, create a new decision tree with the recent 50 day data and combine with previous decision trees (like random forests, but data is not randomised) -- older trees can possibly be down weighted. This would reduce amount of data stored. TODO: Think about correctness guarantees of this strategy.
-
-    # update prices
-    def processPrices(self) -> None:
-        # do not care about max history length for now (TODO)
-        for stock in self.mystocks:
-            self.stored_prices[stock].append(self.today_prices[stock])
-
         
-    def getPreds(self) -> dict:
+    def getPreds(self) -> None:
         # construct target array, which must be in same order as training. NOTE: loops can be removed if using numpy arrays instead of dictionaries, i.e., concatenate the corresponding numpy arrays.
         x = []
         for stock in self.mystocks: 
@@ -135,26 +107,63 @@ class myTrader:
 
         # get predictions for each stock
         for stock in self.mystocks:
-            self.tomorrow_preds[stock] = self.mymodels[stock].predict(x) # TODO: Double check format of this output
+            try: # added this try catch just in case, but probably not needed (i.e., handled in execute day function)
+                self.tomorrow_preds[stock] = self.mymodels[stock].predict(x) # TODO: Double check format of this output
+            except:
+                self.tomorrow_preds[stock] = None
 
-            
-    # always sell everything (simple for now)
+    # initialise day
+    def initDay(self, today_prices) -> None:
+        self.cur_day_count += 1
+        self.yesterday_prices = self.today_prices
+        self.today_prices = today_prices
+        self.today_prices_diff = {}
+
+        # set type of prices, check validity, and compute price difference
+        for stock in self.mystocks:
+            try:
+                self.today_prices[stock] = float(self.today_prices[stock])
+                self.today_prices_diff[stock] = self.today_prices[stock] - self.yesterday_prices[stock]
+            except:
+                print(f"Stock price contians an invalid character(s) and can not convert to float. Removing stock {stock} entirely!")
+                # NOTE: Handled so that all stocks will still be sold.
+
+        # add to price history (do not care about max history length for now -- TODO)
+        for stock in self.mystocks:
+            self.stored_prices[stock].append(self.today_prices[stock])
+
+        if self.analysis:
+            self.mydays_daily.append(self.cur_day_count)
+
+        # reset some variables (for consistency)
+        self.tomorrow_preds = {}
+        self.today_sells = {}
+        self.today_buys = {}
+
+    # simple (for now): always sell everything
     def makeSells(self) -> None:
-        sells = copy.deepcopy(self.cur_stocks_held)
-        for stock in self.mystocks: # NOTE: this for loop could be removed by using numpy arrays instead of dictionaries
+        self.today_sells = copy.deepcopy(self.cur_stocks_held)
+        for stock in self.today_sells.keys(): # NOTE: this for loop could be removed by using numpy arrays instead of dictionaries. NOTE: Using keys of self.today_sells instead of self.my_stocks to cater for the unlikely glitch of price containing an invalid character, i.e., if this occurs, all held stocks of the err stock are still sold.
+
+            # calculate profit for this stock
+            profit_total = self.today_sells[stock]*self.today_prices_diff[stock]
+
+            # HERE!!
+
             if self.analysis:
                 self.mysells_daily[stock].append(self.cur_stocks_held[stock])
+                self.myprofits_daily[stock].append(profit_total)
             
             self.cur_stocks_held[stock] = 0
-        return sells
 
 
-    def makeBuys(self, preds : dict) -> dict:
-        # if buying today and selling tomorrow makes a profit (based on predictions), then buy today
+
+    def makeBuys(self) -> None:
+        # simple (for now): only buy today if predicted to make a profit tomorrow
         # amount to buy will depend on amount of profit made so far on a particular stock
+        pass
 
-        for stock in self.mystocks:
-        HERE!!
+
 
 
 
@@ -163,8 +172,12 @@ class myTrader:
         # and return trades
         pass
 
+
+    # the main function essentially
     def executeDay(self) -> dict:
         self.initDay()
+
+        # check if model exists before making predictions
         pass
 
 
