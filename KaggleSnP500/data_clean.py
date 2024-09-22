76cd2a514c23de5a9e061534b7b33b60dff3ead9
Kheeran <kn16063@bristol.ac.uk>
2024-09-22 18:56:42 +0100

    created and cleaned dataset

--- KaggleSnP500/data_clean.py
+++ KaggleSnP500/data_clean.py
@@ -2,56 +2,74 @@ import pandas as pd
 import os
 import random
 import datetime
+import numpy as np
 
 if __name__ == '__main__':
 
     # consider only dates in 2019,2020,2021
     dates = []
     start_date = datetime.date(2019, 1, 1) 
-    end_date = datetime.date(2021, 12, 31)    # perhaps date.today()
+    end_date = datetime.date(2021, 12, 31) 
     delta = end_date - start_date   # returns timedelta
     for i in range(delta.days + 1):
         dates.append(str(start_date + datetime.timedelta(days=i)))
 
-    # dicts for open price 
-    prices = {}
+    # dicts for open, high, low, close, and volume 
+    stocks_info = {}
+
+    # define main columns and helper label function
+    main_cols = ['Open', 'High', 'Low', 'Close', 'Volume']
+    mylabel = lambda a,b : f'{a}_{b}'
+
+
     for root, _, files in os.walk('./archive'):
         # randomly sample ~100 stocks to work with (fix local randomness with seed)
         random.Random(125).shuffle(files)
 
-        for file in files[:100]:
+        for file in files[:10]:
             df = pd.read_csv(os.path.join(root, file))
             stock = file[:3]
 
-            # extract open, high, low, close, and volume (just open price for now) 
+            # retain rows with relevant dates
             df = df.loc[df['Date'] >= str(start_date)]
             df = df.loc[df['Date'] <= str(end_date)]
+            stock_dates = df['Date'].values
 
-            # set up local columns
-            this_dates = df['Date'].values
-            this_opens = df['Open'].values
-            # this_highs = df['High'].values
-            # this_lows = df['Low'].values
-            # this_closes = df['Close'].values
-            # this_volumes = df['Volume'].values
+            # get values of relevant columns
+            stock_info = df[main_cols].values
 
-            # prep stock in dictionary
-            prices[stock] = []
+            # prep relevant arrays in dictionaries
+            for key in main_cols:
+                stocks_info[mylabel(stock,key)] = []
 
-            # make dates line up and fill spaces with None
+            # make dates line up (without any gaps) and fill spaces with None
             i = 0
-            for j in range(len(this_dates)):
-                if dates[i] == this_dates[j]:
-                    prices[stock].append(this_opens[j])
+            for j in range(len(stock_dates)):
+                if dates[i] == stock_dates[j]:
+                    for k, key in enumerate(main_cols):
+                        stocks_info[mylabel(stock,key)].append(stock_info[j][k])
                     i += 1
                 else:
-                    while dates[i] != this_dates[j]:
-                        prices[stock].append(None)
+                    while dates[i] != stock_dates[j]:
+                        for key in main_cols:
+                            stocks_info[mylabel(stock,key)].append(None)
                         i += 1
-                    prices[stock].append(this_opens[j])
+                    for k, key in enumerate(main_cols):
+                        stocks_info[mylabel(stock,key)].append(stock_info[j][k])
                     i += 1
 
-            
+    # construct main dataframe
+    main_df = pd.DataFrame.from_dict(stocks_info)
+    main_df.insert(0, 'Date', dates)
+    # print(main_df.head())
 
-    for key in prices.keys():
-        print(prices[key])
+    # only keep columns with more non-Nan entries than its median and then remove rows with any NaN entry
+    non_na_sum = main_df.notna().sum()
+    med = non_na_sum.median()
+    good_cols = non_na_sum.loc[non_na_sum >= med].index
+    main_df = main_df[good_cols].dropna()
+
+    print(main_df.head())
+
+
+
