04c517617dd18c79cf91794e0093d3b3a99a246d
Kheeran <kn16063@bristol.ac.uk>
2024-09-26 09:29:18 +0100

    quick scipy refresher

--- Others/refresher.py
+++ Others/refresher.py
@@ -1,16 +1,28 @@
-def mergeAlternately(word1, word2):
-    word_len = len(word1)
-    if len(word1) > len(word2):
-        word_len = len(word2)
+from scipy.stats import rv_continuous
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
-    out_word = ''
+class gaussian_gen(rv_continuous):
+    # Normal Distribution
+    def _pdf(self,x):
+        return np.exp(-x**2/2) / np.sqrt(2 * np.pi)
 
-    for i in range(word_len):
-        out_word += word1[i]
-        out_word += word2[i]
+# initialise gaussian noise dist
+noise = gaussian_gen()
 
+# generate linear data with some noise
+m = 2.6
+xs = np.array([i + 1 for i in range(0,100,4)])
+ys = np.array([m*x + noise.rvs(scale=5) for x in xs])
 
+# plot data
+plt.figure()
+plt.plot(xs, ys, label="observed")
 
-    return out_word + word1[word_len:] + word2[word_len:]
+# fit linear regression
+reg = LinearRegression(fit_intercept=False).fit(xs.reshape(-1,1), ys)
+plt.plot(xs, reg.predict(xs.reshape(-1,1)), label="regression")
+plt.show()
 
-print(mergeAlternately('abc', 'pqr'))
+print(reg.coef_, reg.score(xs.reshape(-1,1), ys))
